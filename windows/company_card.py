aaa8a5e3c34d8bbb4512934282698f7fc5effec9
Ivan <ideshchenko@yandex.ru>
2026-04-17 16:13:33 +0300

    add ui files

--- windows/company_card.py
+++ windows/company_card.py
@@ -1,35 +1,17 @@
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QFrame, QLabel, QVBoxLayout
 from PyQt6.QtCore import Qt, pyqtSignal
-from PyQt6.QtWidgets import QSizePolicy
+from py_ui.company_card import Ui_Frame
 
 
-class CompanyCard(QFrame):
+class CompanyCard(Ui_Frame, QFrame):
 	clicked = pyqtSignal(dict)
 	def __init__(self, company):
 		super().__init__()
+		self.setupUi(self)
 		self.company = company
 		self.setup_ui()
 
-	def setup_ui(self):
-		self.setFrameStyle(QFrame.Shape.Box | QFrame.Shadow.Plain)
-		layout = QVBoxLayout()
-		name_label = QLabel(self.company["name"])
-		name_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-		layout.addWidget(name_label)
-
-		image_label = QLabel()
-		pixmap = QPixmap(f"images/{self.company['logo']}").scaled(100, 100)
-		image_label.setPixmap(pixmap)
-
-		layout.addWidget(image_label)
-
-		contribution_label = QLabel(f" Взнос {self.company['contribution_price']} р.")
-		contribution_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-		layout.addWidget(contribution_label)
-
-		self.setLayout(layout)
-	
 	def mousePressEvent(self, a0) -> None:
 		self.clicked.emit(self.company)
 		super().mousePressEvent(a0)
